b3932c2a40303c33bff674bc564070734bcbdadd
李騰 <liteng@basenet.co.jp>
2021-12-20 14:25:37 +0900

    homepage 20%

--- src/components/common/AppBar.vue
+++ src/components/common/AppBar.vue
@@ -1,15 +1,36 @@
 <template>
-  <v-app-bar app>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-     <v-toolbar-title>Application</v-toolbar-title>
-    <v-text-field
-      hide-details
-      prepend-icon="mdi-magnify"
-      single-line
-      
-    ></v-text-field>
-   
-  </v-app-bar>
+    <v-app-bar
+      app
+      color="white"
+      flat
+    >
+      <v-container class="py-0 fill-height">
+        <v-avatar
+          class="mr-10"
+          color="grey darken-1"
+          size="32"
+        ></v-avatar>
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          text
+        >
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+        <v-responsive max-width="260">
+          <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
 </template>
 
 <script>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,47 +1,20 @@
 <template>
   <v-app id="inspire">
-    <v-system-bar app>
-      <v-spacer></v-spacer>
-
-      <v-icon>mdi-square</v-icon>
-
-      <v-icon>mdi-circle</v-icon>
-
-      <v-icon>mdi-triangle</v-icon>
-    </v-system-bar>
-
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      temporary
-    >
+    <AppBar />
+    <v-navigation-drawer v-model="drawer"  fixed temporary>
       <!--  -->
     </v-navigation-drawer>
 
     <v-main class="grey lighten-2">
       <v-container>
+        <Carousel/>
         <v-row>
           <template v-for="n in 4">
-            <v-col
-              :key="n"
-              class="mt-2"
-              cols="12"
-            >
+            <v-col :key="n" class="mt-2" cols="12">
               <strong>Category {{ n }}</strong>
             </v-col>
 
-            <v-col
-              v-for="j in 6"
-              :key="`${n}${j}`"
-              cols="6"
-              md="2"
-            >
+            <v-col v-for="j in 6" :key="`${n}${j}`" cols="6" md="2">
               <v-sheet height="150"></v-sheet>
             </v-col>
           </template>
@@ -52,7 +25,14 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({ drawer: null }),
-  }
+import AppBar from ".././components/common/AppBar.vue";
+import Carousel from ".././components/homepage/Carousel.vue"
+
+export default {
+  data: () => ({ drawer: null }),
+  components: {
+    AppBar,
+    Carousel
+  },
+};
 </script>
