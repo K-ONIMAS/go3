47c9a05f446a0cae73c8c52ef0b6a6568e82fd09
李騰 <liteng@basenet.co.jp>
2021-12-20 12:42:29 +0900

    homepage 20%

---
+++ src/components/common/AppBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar app>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+     <v-toolbar-title>Application</v-toolbar-title>
+    <v-text-field
+      hide-details
+      prepend-icon="mdi-magnify"
+      single-line
+      
+    ></v-text-field>
+   
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
